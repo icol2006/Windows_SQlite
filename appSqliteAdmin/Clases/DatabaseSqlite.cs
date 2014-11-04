b2d6d2fa5d8e1d79aa21853a1640666fd690a6aa
icol2006 <icol2006@hotmail.com>
2014-11-03 21:59:12 -0600

    Update DatabaseSqlite.cs

--- appSqliteAdmin/Clases/DatabaseSqlite.cs
+++ appSqliteAdmin/Clases/DatabaseSqlite.cs
@@ -1,3 +1,13 @@
+﻿#region Copyright & License Information
+﻿/*
+ * Copyright 2014 Maykol Alvarado P
+ * This file is part of windows sqlite which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+﻿#endregion
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
